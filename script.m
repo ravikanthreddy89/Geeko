da97a03d02b42a4597bcbde5d55abfda08415886
Ravikanth Reddy <ravikanthreddy89@gmail.com>
2014-07-25 09:40:21 -0400

    Create script.m

---
+++ script.m
@@ -0,0 +1,17 @@
+/*Load the data*/
+train_data=load('train.csv');
+test_data=load('test.csv');
+
+/*Separate label and datapoints*/
+train_label=train_data(:,1);
+train_data=train_data(:,2:end);
+
+/*Dimensionality Reduction*/
+
+
+/*Training*/
+
+
+/*Testing*/
+
+/*Save the parameters*/
